9af36c6b7dd83a246ea93cde6516499a802d0de6
Cortes Tomás <cortestomas2727@gmail.com>
2025-08-26 20:05:10 +0000

    Punto3

---
+++ Hola.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Hola {
+    Scanner scanner = new Scanner(System.in);
+    int A;
+    A = Scanner.nextInt();
+    public static void main (String[] args){
+        System.out.println("HOLA" + A);
+    }
+}
---
+++ TP°1/Intercambio.java
@@ -0,0 +1,16 @@
+
+public class Punto3 {
+    public static void main(String[] args) {
+    int A, B, C;
+    A = 1;
+    B = 2;
+    C = 3;
+    B = A;
+    A = C;
+    C = B;
+    System.out.println("Los resultados son:");
+    System.out.println("A = " + A);
+    System.out.println("B = " + B);
+    System.out.println("C = " + C);
+    }
+}
